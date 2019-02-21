9422a0002d745a94e7a65042dbf8ebc81c184bff
sgargula <sylwia.gargula@wp.pl>
2019-02-21 11:58:49 +0100

    new

--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"github.com/Appliscale/cloud-security-audit/cmd"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	fmt.Println("Received body: ", request.Body)
 	cmd.Execute()
 	return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 200}, nil
 }
